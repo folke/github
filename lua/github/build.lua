a15bb25a34f6ee03f1dbd027a135688dd475ec42
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-05 19:04:23 +0200

    chore: updates

--- lua/github/build.lua
+++ lua/github/build.lua
@@ -3,110 +3,128 @@ local Util = require("github.util")
 local M = {}
 
 M.keep = {
-	"tests/minit.lua",
-	"LICENSE.md",
+  "tests/minit.lua",
+  "LICENSE.md",
 }
 
 M.props = {
-	["LazyVim"] = {
-		spec = '{ "LazyVim/LazyVim", import = "lazyvim.plugins" },',
-	},
-	["lazy.nvim"] = { spec = "" },
+  ["LazyVim"] = {
+    spec = '{ "LazyVim/LazyVim", import = "lazyvim.plugins" },',
+    feature_request = {
+      top = [[
+  - type: markdown
+    attributes:
+      value: |
+        **IMPORTANT:** ideas or requests related to extras, should go in the [Extra Requests](https://github.com/LazyVim/LazyVim/discussions/categories/extra-requests)
+        discussion forum.
+      ]],
+      checks = [[
+        - label: This is not a request for a new or existing extra (see above)
+          required: true
+      ]],
+    },
+  },
+  ["lazy.nvim"] = { spec = "" },
 }
 
 M.repos = {
-	"LazyVim/LazyVim",
-	"folke/drop.nvim",
-	"folke/edgy.nvim",
-	"folke/flash.nvim",
-	"folke/lazydev.nvim",
-	"folke/lazy.nvim",
-	"folke/neoconf.nvim",
-	"folke/neodev.nvim",
-	"folke/noice.nvim",
-	"folke/persistence.nvim",
-	"folke/styler.nvim",
-	"folke/todo-comments.nvim",
-	"folke/tokyonight.nvim",
-	"folke/trouble.nvim",
-	"folke/ts-comments.nvim",
-	"folke/which-key.nvim",
-	"folke/zen-mode.nvim",
+  "LazyVim/LazyVim",
+  "folke/drop.nvim",
+  "folke/edgy.nvim",
+  "folke/flash.nvim",
+  "folke/github",
+  "folke/lazydev.nvim",
+  "folke/lazy.nvim",
+  "folke/neoconf.nvim",
+  "folke/neodev.nvim",
+  "folke/noice.nvim",
+  "folke/persistence.nvim",
+  "folke/styler.nvim",
+  "folke/todo-comments.nvim",
+  "folke/tokyonight.nvim",
+  "folke/trouble.nvim",
+  "folke/ts-comments.nvim",
+  "folke/which-key.nvim",
+  "folke/zen-mode.nvim",
 }
 
 ---@param dir string
 ---@param props table
 function M.templates(dir, props)
-	local have_tests = vim.uv.fs_stat(dir .. "/tests") ~= nil
-	local have_contributing = vim.uv.fs_stat(dir .. "/CONTRIBUTING.md") ~= nil
-	if have_contributing then
-		props.checklist = vim.trim([[
+  local have_tests = vim.uv.fs_stat(dir .. "/tests") ~= nil
+  local have_contributing = vim.uv.fs_stat(dir .. "/CONTRIBUTING.md") ~= nil
+  if have_contributing then
+    props.checklist = vim.trim([[
 ## Checklist
 
 - [ ] I've read the [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md) guidelines.
     ]])
   else
     props.checklist = ""
-	end
-	props.tests = tostring(have_tests)
-	local tpl_dir = vim.fs.normalize(Util.me .. "/templates")
-	vim.fs.find(function(name, path)
-		local tpl = (path .. "/" .. name):sub(#tpl_dir + 2)
-		if vim.tbl_contains(M.keep, tpl) and vim.uv.fs_stat(dir .. "/" .. tpl) then
-			print(" - skipping", tpl)
-			return false
-		end
+  end
+  props.tests = tostring(have_tests)
+  local tpl_dir = vim.fs.normalize(Util.me .. "/templates")
+  vim.fs.find(function(name, path)
+    local tpl = (path .. "/" .. name):sub(#tpl_dir + 2)
+    if vim.tbl_contains(M.keep, tpl) and vim.uv.fs_stat(dir .. "/" .. tpl) then
+      print(" - skipping", tpl)
+      return false
+    end
 
     if tpl:find(".github") and dir:find("/github$") then
       print(" - skipping", tpl)
       return false
     end
 
-		if (tpl:find("scripts") or tpl:find("tests")) and not have_tests then
-			print(" - skipping", tpl)
-			return false
-		end
-		props = vim.deepcopy(props)
-		for k, v in pairs(M.props[props.name] or {}) do
-			props[k] = v
-		end
-		if not props.spec then
-			props.spec = ([[{ "%s", opts = {} },]]):format(props.repo)
-		end
-		Util.template(dir, tpl, props)
-		if tpl:find("scripts") then
-			vim.uv.fs_chmod(dir .. "/" .. tpl, tonumber("755", 8))
-		end
-	end, { path = tpl_dir, type = "file" })
+    if (tpl:find("scripts") or tpl:find("tests")) and not have_tests then
+      print(" - skipping", tpl)
+      return false
+    end
+    props = vim.deepcopy(props)
+    for k, v in pairs(M.props[props.name] or {}) do
+      props[k] = v
+    end
+    if not props.spec then
+      props.spec = ([[{ "%s", opts = {} },]]):format(props.repo)
+    end
+    props.feature_request = vim.tbl_deep_extend("keep", props.feature_request or {}, {
+      top = "",
+      checks = "",
+    })
+    Util.template(dir, tpl, props)
+    if tpl:find("scripts") then
+      vim.uv.fs_chmod(dir .. "/" .. tpl, tonumber("755", 8))
+    end
+  end, { path = tpl_dir, type = "file" })
 end
 
 function M.rm(dir, file)
-	local path = dir .. "/" .. file
-	if vim.uv.fs_stat(path) then
-		print(" - removing", path)
-		vim.uv.fs_unlink(path)
-	end
+  local path = dir .. "/" .. file
+  if vim.uv.fs_stat(path) then
+    print(" - removing", path)
+    vim.uv.fs_unlink(path)
+  end
 end
 
 function M.update(repo)
-	print("updating", repo)
-	local parts = vim.split(repo, "/")
-	assert(#parts == 2, "Invalid repo: " .. repo)
+  print("updating", repo)
+  local parts = vim.split(repo, "/")
+  assert(#parts == 2, "Invalid repo: " .. repo)
 
-	local dir = vim.fs.normalize("~/projects/" .. parts[2])
+  local dir = vim.fs.normalize("~/projects/" .. parts[2])
 
-	assert(vim.uv.fs_stat(dir), "Directory does not exist: " .. dir)
+  assert(vim.uv.fs_stat(dir), "Directory does not exist: " .. dir)
 
-	M.rm(dir, "tests/run")
-	M.rm(dir, "tests/init.lua")
+  M.rm(dir, "tests/run")
+  M.rm(dir, "tests/init.lua")
 
-	M.templates(dir, { name = parts[2], repo = repo })
+  M.templates(dir, { name = parts[2], repo = repo })
 end
 
 function M.build()
-	for _, repo in ipairs(M.repos) do
-		M.update(repo)
-	end
+  for _, repo in ipairs(M.repos) do
+    M.update(repo)
+  end
 end
 M.build()
 
--- lua/github/util.lua
+++ lua/github/util.lua
@@ -3,47 +3,51 @@ local M = {}
 M.me = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h:h:h")
 
 function M.read(file)
-	local fd = assert(io.open(file, "r"))
-	---@type string
-	local data = fd:read("*a")
-	fd:close()
-	return data
+  local fd = assert(io.open(file, "r"))
+  ---@type string
+  local data = fd:read("*a")
+  fd:close()
+  return data
 end
 
 function M.write(file, contents)
-	local fd = assert(io.open(file, "w+"))
-	fd:write(contents)
-	fd:close()
+  local fd = assert(io.open(file, "w+"))
+  fd:write(contents)
+  fd:close()
 end
 
 ---@param str string template string
 ---@param props table key value pairs to replace in the string
 function M._template(str, props)
-	local lines = vim.split(str, "\n")
-	lines = vim.tbl_map(function(line)
-		return line:gsub("(%$%b{})", function(w)
-			local ret = vim.tbl_get(props, unpack(vim.split(w:sub(3, -2), ".", { plain = true })))
-			assert(ret, "Property not found: " .. w)
-			return ret
-		end)
-	end, lines)
-	return table.concat(lines, "\n")
+  return str:gsub("(\n?[ \t]*%$%b{}[ \t]*\n?)", function(w)
+    local pre, var, post = w:match("^(%s*)%${(.-)}(%s*)$")
+    local ret = vim.tbl_get(props, unpack(vim.split(var, ".", { plain = true })))
+    assert(ret, "Property not found: " .. var)
+    if ret == "" and pre:find("^\n") and post:find("\n$") then
+      return "\n"
+    end
+    if ret:match("%s*\n%s*$") or ret:match("^%s*$") then
+      post = ""
+      ret = ret:gsub("%s*\n%s*$", "\n")
+    end
+    return pre .. ret .. post
+  end)
 end
 
 ---@param dir string
 ---@param tpl string
 ---@param props table
 function M.template(dir, tpl, props)
-	dir = vim.fs.normalize(dir)
-	local fin = vim.fn.fnamemodify(M.me .. "/templates/" .. tpl, ":p")
-	assert(vim.uv.fs_stat(fin), "Template does not exist: " .. fin)
+  dir = vim.fs.normalize(dir)
+  local fin = vim.fn.fnamemodify(M.me .. "/templates/" .. tpl, ":p")
+  assert(vim.uv.fs_stat(fin), "Template does not exist: " .. fin)
 
-	local fout = vim.fn.fnamemodify(dir .. "/" .. tpl, ":p")
-	vim.fn.mkdir(vim.fn.fnamemodify(fout, ":p:h"), "p")
+  local fout = vim.fn.fnamemodify(dir .. "/" .. tpl, ":p")
+  vim.fn.mkdir(vim.fn.fnamemodify(fout, ":p:h"), "p")
 
-	local data = M.read(fin)
-	data = M._template(data, props)
-	M.write(fout, data)
+  local data = M.read(fin)
+  data = M._template(data, props)
+  M.write(fout, data)
 end
 
 return M
