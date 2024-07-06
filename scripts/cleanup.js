169a7caf15372b9660e737e5bf16ed73ccd6ac41
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-06 09:18:06 +0200

    feat(cleanup): closes issues/PRS & discussions

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,45 +1,64 @@
 module.exports = async ({ github }) => {
   const PER_PAGE = 100;
   let page = 1;
-  let hasMoreNotifications = true;
+  let before = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
 
-  while (hasMoreNotifications) {
+  do {
     // Fetch notifications for the current page
     const notifs = await github.request("GET /notifications", {
       per_page: PER_PAGE,
       page: page,
+      // before: before.toISOString(),
     });
-
-    if (notifs.data.length === 0) {
-      hasMoreNotifications = false;
-      console.log("No more notifications to process.");
-      break;
-    }
 
     // Loop through each notification and its corresponding ID
     for (const notif of notifs.data) {
-      const latestCommentUrl = notif.subject.latest_comment_url;
+      let done = false;
 
-      if (latestCommentUrl) {
-        // Fetch the comment details
-        const comment = await github.request(`GET ${latestCommentUrl}`);
-        const isStaleBot =
-          comment.data.user.login === "github-actions[bot]" &&
-          /stale/.test(comment.data.body);
+      // Mark discussions as done
+      if (notif.subject.type == "Discussion") {
+        done = true;
+      }
 
-        // If the comment was made by stale-bot, delete the notification
-        if (isStaleBot) {
-          console.log(`Deleting notification: ${notif.url}`);
-          await github.request(`DELETE /notifications/threads/${notif.id}`);
-        } else {
-          console.log(
-            `Notification is not from stale-bot ${notif.id} ${latestCommentUrl}`,
-          );
+      // Mark closes isssues / RRs as done
+      else if (
+        notif.subject.type == "Issue" ||
+        notif.subject.type == "PullRequest"
+      ) {
+        const latestCommentUrl = notif.subject.latest_comment_url;
+        // Fetch the issue/PR details
+        const details = await github.request(`GET ${notif.subject.url}`);
+        // Mark as done if the issue/PR is closed
+        if (details.data.state === "closed") done = true;
+        // If the issue/PR is open, check if the latest comment is from stale-bot
+        else if (latestCommentUrl) {
+          // Fetch the comment details
+          const comment = await github.request(`GET ${latestCommentUrl}`);
+          const isStaleBot =
+            comment.data.user.login === "github-actions[bot]" &&
+            /stale/.test(comment.data.body);
+
+          // If the comment was made by stale-bot, delete the notification
+          if (isStaleBot) {
+            done = true;
+            console.log(`Deleting notification: ${notif.url}`);
+            await github.request(`DELETE /notifications/threads/${notif.id}`);
+          } else {
+            console.log(
+              `Notification is not from stale-bot ${notif.id} ${latestCommentUrl}`,
+            );
+          }
         }
+      }
+      if (done) {
+        console.log(`Deleting notification: ${notif.url}`);
+        await github.request(`DELETE /notifications/threads/${notif.id}`);
+      } else {
+        console.log(`Skipping notifications ${notif.url}`);
       }
     }
 
     // Move to the next page
     page++;
-  }
+  } while (notifs.data.length === PER_PAGE);
 };
