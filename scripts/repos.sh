e11ae555120cc462f6dbcb8d66bbd2799c1b5488
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-06 16:26:57 +0200

    fix: repos

---
+++ scripts/repos.sh
@@ -0,0 +1,22 @@
+export REPOS="
+  LazyVim
+  drop.nvim
+  edgy.nvim
+  flash.nvim
+  github
+  lazydev.nvim
+  lazy.nvim
+  neoconf.nvim
+  neodev.nvim
+  noice.nvim
+  paint.nvim
+  persistence.nvim
+  styler.nvim
+  todo-comments.nvim
+  tokyonight.nvim
+  trouble.nvim
+  twilight.nvim
+  ts-comments.nvim
+  which-key.nvim
+  zen-mode.nvim
+"
