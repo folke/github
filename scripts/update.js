190b6317bf821cee27276c5057cbc3cb84d57fcd
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-06 16:14:40 +0200

    ci: repo updates

--- scripts/update.js
+++ scripts/update.js
@@ -1,37 +1,50 @@
 // @ts-check
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, context }) => {
-  const repo = `${context.repo.owner}/${context.repo.repo}`;
-
-  // Enable repository settings
-  await github.rest.repos.update({
-    ...context.repo,
-    allow_update_branch: true,
-    has_discussions: true,
-    allow_squash_merge: true,
-    allow_rebase_merge: true,
-    allow_merge_commit: false,
-    has_wiki: true,
-    squash_merge_commit_title: "PR_TITLE",
-    squash_merge_commit_message: "PR_BODY",
+  const repos = await github.request("GET /users/folke/repos", {
+    per_page: 100,
+    type: "owner",
   });
 
-  // Update branch protection
-  await github.request(`PUT /repos/${repo}/branches/main/protection`, {
-    headers: {
-      accept: "application/vnd.github.v3+json",
-    },
-    allow_deletions: false,
-    allow_force_pushes: false,
-    allow_fork_syncing: false,
-    block_creations: false,
-    enforce_admins: false,
-    lock_branch: false,
-    required_conversation_resolution: false,
-    required_linear_history: false,
-    required_signatures: false,
-    required_status_checks: null,
-    required_pull_request_reviews: null,
-    restrictions: null,
-  });
+  for (const repo of repos.data) {
+    const [owner, name] = repo.full_name.split("/");
+    if (!name.includes("nvim")) continue;
+
+    console.log(`Updating ${repo.full_name}...`);
+    // Enable repository settings
+    await github.rest.repos.update({
+      repo: name,
+      owner: owner,
+      allow_update_branch: true,
+      has_discussions: true,
+      allow_squash_merge: true,
+      allow_rebase_merge: true,
+      allow_merge_commit: false,
+      has_wiki: true,
+      squash_merge_commit_title: "PR_TITLE",
+      squash_merge_commit_message: "PR_BODY",
+    });
+
+    // Update branch protection
+    await github.request(
+      `PUT /repos/${owner}/${name}/branches/main/protection`,
+      {
+        headers: {
+          accept: "application/vnd.github.v3+json",
+        },
+        allow_deletions: false,
+        allow_force_pushes: false,
+        allow_fork_syncing: false,
+        block_creations: false,
+        enforce_admins: false,
+        lock_branch: false,
+        required_conversation_resolution: false,
+        required_linear_history: false,
+        required_signatures: false,
+        required_status_checks: null,
+        required_pull_request_reviews: null,
+        restrictions: null,
+      },
+    );
+  }
 };
